d5a451cf76b9c1368ce86db36d6ec0c8fd1b997a
Rudi Giesler <rudi@reachdigitalhealth.org>
2026-04-15 14:44:09 +0200

    Add Ruff

--- synch/apps.py
+++ synch/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SynchConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'synch'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "synch"
