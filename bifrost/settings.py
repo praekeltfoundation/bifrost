4ac04e288cfe4ff870c59d49ce97b80fd0774fb0
Rudi Giesler <rudi@reachdigitalhealth.org>
2026-04-15 14:47:15 +0200

    Add mypy

--- bifrost/settings.py
+++ bifrost/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-s4+q=o=w&5-e)h+fw@xbyzlgcxavz8u$^_wcto8uu+)splw3d2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
